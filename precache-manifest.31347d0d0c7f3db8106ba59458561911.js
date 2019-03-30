777775db7c97536701587367db1ffbcd1be82f3a
bflickinger <barry.flickinger@gmail.com>
2019-03-30 07:28:18 -0700

    Updates

---
+++ precache-manifest.31347d0d0c7f3db8106ba59458561911.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5e0ea49c1b8930589293690d4023af0",
+    "url": "/clickygame/static/media/Cinnamon-Apple-Filled_200.b5e0ea49.png"
+  },
+  {
+    "revision": "450b25ddfd34db237b18",
+    "url": "/clickygame/static/css/main.93517cec.chunk.css"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "973eca460e14e0ddebe0",
+    "url": "/clickygame/static/js/2.693cc7d6.chunk.js"
+  },
+  {
+    "revision": "8b95cf665c7c04a66cb24656f3baacd3",
+    "url": "/clickygame/static/media/Birthday-Batter_200.8b95cf66.png"
+  },
+  {
+    "revision": "44c7076c3cc813c56f05bbca7194bf50",
+    "url": "/clickygame/static/media/Choc-Iced-Ring-Doughnut-sprinkles_200.44c7076c.png"
+  },
+  {
+    "revision": "49a3a83cbfc187075b991edb4cfe2e54",
+    "url": "/clickygame/static/media/Chocolate-Cake-Doughnut_200.49a3a83c.png"
+  },
+  {
+    "revision": "b984612f21c19d65e7c9e272b4e4d2c3",
+    "url": "/clickygame/static/media/Chocolate-Iced-Kreme-filled_200.b984612f.png"
+  },
+  {
+    "revision": "d7755a4903e38a18d70743e402bf7982",
+    "url": "/clickygame/static/media/Chocolate-Iced-Ring_200.d7755a49.png"
+  },
+  {
+    "revision": "affc72f27f18df3cfde08a18e47862ce",
+    "url": "/clickygame/static/media/Chocolate-iced-glazed_200.affc72f2.png"
+  },
+  {
+    "revision": "450b25ddfd34db237b18",
+    "url": "/clickygame/static/js/main.3d16016b.chunk.js"
+  },
+  {
+    "revision": "351f4b31fca3679a23eda54d9ba618bd",
+    "url": "/clickygame/static/media/Cinnamon-Roll-Doughnut_200.351f4b31.png"
+  },
+  {
+    "revision": "b4ded6f34ec929d15fb22686f90aede5",
+    "url": "/clickygame/static/media/Cinnamon-Sugar-Ring_200.b4ded6f3.png"
+  },
+  {
+    "revision": "aca9e544dc2da8b1db648b504db5b89d",
+    "url": "/clickygame/static/media/Double-Dark-Chocolate_200.aca9e544.png"
+  },
+  {
+    "revision": "f8d3269ca48abdc48f16702fc91d6027",
+    "url": "/clickygame/static/media/Glazed-Cruller_200.f8d3269c.png"
+  },
+  {
+    "revision": "3ceac9cdecdacd61af068f78a486bfdc",
+    "url": "/clickygame/static/media/Glazed-Raspberry-Filled-Doughnut_200.3ceac9cd.png"
+  },
+  {
+    "revision": "9dfe5a90aa74e06ba717fe7cd8fcbe1e",
+    "url": "/clickygame/static/media/Oreo-Cookies-and-Kreme_200.9dfe5a90.png"
+  },
+  {
+    "revision": "63230282c687d727372e74108be25a7a",
+    "url": "/clickygame/static/media/OriginalGlazed_200.63230282.png"
+  },
+  {
+    "revision": "f29c26fea3ed7b4f920e534c7c02aad0",
+    "url": "/clickygame/static/media/Strawberry-Iced-with-sprinkles_200.f29c26fe.png"
+  },
+  {
+    "revision": "63d521063c79995045e0ffbc7bcc29e4",
+    "url": "/clickygame/static/media/Traditional-Cake_200.63d52106.png"
+  },
+  {
+    "revision": "e8d0f64fc5e88eb052110d09920a38a4",
+    "url": "/clickygame/index.html"
+  }
+];
